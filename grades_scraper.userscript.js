4fdcf718b7ef61804424c0b9c10b7a6086ae68b6
ewen-lbh <ewen.lebihan7@gmail.com>
2018-12-07 17:16:16 +0100

    added observer (not functionnal, just debug, but working)

--- grades_scraper.userscript.js
+++ grades_scraper.userscript.js
@@ -104,9 +104,6 @@ function main() {
 }
 main()
 show()
-rerunOnClassChange(profile.activeTrimesterDOMElement)
-
-
 
 
 function sanitizeGrade(str) {
@@ -194,10 +191,33 @@ function getSubSubjectParent(ele) {
 }
 
 function show() {
+    var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
     console.log('===PROFILE===')
-    console.table(profile)
+    if(!isFirefox) {
+        console.table(profile)
+    } else {
+        console.log('nthTrimester:')
+        console.log(profile.nthTrimester)
+        console.log('conseilDeClasseDate:')
+        console.log(profile.conseilDeClasseDate)
+        console.log('activeTrimesterDOMElement:')
+        console.log(profile.activeTrimesterDOMElement)
+    }
     console.log('===GRADES===')
-    console.table(grades)
+    if(!isFirefox) {
+        console.table(grades)
+    } else {
+        console.log('values:')
+        console.log(grades.values)
+        console.log('subject:')
+        console.log(grades.subject)
+        console.log('subjectTeacher:')
+        console.log(grades.subjectTeacher)
+        console.log('subjectAvg:')
+        console.log(grades.subjectAvg)
+        console.log('globalAvg:')
+        console.log(grades.globalAvg)
+    }
     console.log('Vous êtes sur le trimestre n°'+profile.nthTrimester)
 }
 
@@ -253,3 +273,27 @@ function sum(input) {
     }
     return total;
 }
+
+// Select the node that will be observed for mutations
+var targetNode = profile.activeTrimesterDOMElement
+
+// Options for the observer (which mutations to observe)
+var config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+var callback = function(mutationsList, observer) {
+    for(var mutation of mutationsList) {
+        if (mutation.type == 'attributes') {
+            console.log('The ' + mutation.attributeName + ' attribute was modified.');
+        }
+    }
+};
+
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
+// Later, you can stop observing
+// observer.disconnect();
